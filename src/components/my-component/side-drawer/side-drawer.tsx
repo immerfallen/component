c69a3b8b7495603e60aeb4047b714fa8276af347
Maro de Melo <maromelo@tempus.digital>
2021-11-26 16:14:57 -0300

    first commit

---
+++ src/components/my-component/side-drawer/side-drawer.tsx
@@ -0,0 +1,16 @@
+import {h, Component} from "@stencil/core";
+
+@Component({
+    tag: 'side-drawer'
+}) 
+
+
+export class SideDrawer {
+    render(){
+        return (
+        <div>
+            <h1>The Side Drawer</h1>
+            </div>
+        )
+    };
+}
